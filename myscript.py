ede600d9bd3fea4e6c4c5f07d58f1eacbee5bcc7
XxshimxX <89857605+XxshimxX@users.noreply.github.com>
2024-03-18 11:46:33 -0400

    modifé myscript et main.yml pour automatisation

--- myscript.py
+++ myscript.py
@@ -1,7 +1,10 @@
 import os
 
-bon_commit = input("Indiquer le hash du dernier commit fonctionnel : ")
-mauvais_commit = input("Indiquer le hash du commit non-fonctionnel : ")
+## bon_commit = input("Indiquer le hash du dernier commit fonctionnel : ")
+## mauvais_commit = input("Indiquer le hash du commit non-fonctionnel : ")
+
+bon_commit = sys.argv[1]
+mauvais_commit = sys.argv[2]
 
 os.system(f"git bisect start {mauvais_commit} {bon_commit}")
 os.system(f"git bisect run python manage.py test")
